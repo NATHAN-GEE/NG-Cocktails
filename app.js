cfc1255f74c3f69fbb8cd5f8b859cd519de904b9
FirstName LastName <nathangee867@gmail.com>
2021-07-02 09:26:18 -0400

    update v1.3

--- app.js
+++ app.js
@@ -109,3 +109,11 @@ let searchURL = async function () {
 };
 
 search.addEventListener("click", searchURL);
+
+//------------------------------About Modal---------------------------
+var myModal = document.getElementById("myModal");
+var myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", function () {
+  myInput.focus();
+});
